ecfe033254006c4ab5c3ff28df59fe26cf286d7b
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2026-07-22 22:05:21 +0100

    feat: add fetch to get profile

--- src/App.jsx
+++ src/App.jsx
@@ -25,3 +25,108 @@ function App() {
 }
 
 export default App;
+
+// const chats = [
+//   {
+//     cuid: 1,
+//     subject: "Yids chat",
+//     users: [
+//       {
+//         // id: 1,
+//         username: "m@m.net",
+//         name: "Mine",
+//         bio: "The first if statement will now execute and return a <div> with the text “Loading…” This is often the case when you are fetching from an API, since it might take some time to actually retrieve the data, it is good practice to show an indicator for that. If none of those checks passed, then we have t",
+//       },
+//       {
+//         // cuid: 2,
+//         username: "m3@m.net",
+//         name: "Mine3",
+//         bio: null,
+//       },
+//     ],
+//     messages: [
+//       {
+//         cuid: 4,
+//         senderId: 1,
+//         content:
+//           "In our <List /> component, we have two if statements acting as a guard that immediately returns an element based on the condition.",
+//         createdAt: "2026-07-12T17:48:37.168Z",
+//       },
+//       {
+//         cuid: 5,
+//         senderId: 1,
+//         content:
+//           "One is to check if the property animals exists, and the other is to check if the length of the list is greater than 0. In this case, our list is empty, so the second if statement executes, which immediately returns the <div> element that contains the text “There are no animals in the list”.",
+//         createdAt: "2026-07-13T18:48:37.168Z",
+//       },
+//       {
+//         cuid: 6,
+//         senderId: 2,
+//         content: "If we remove the animals property:",
+//         createdAt: "2026-07-14T19:48:37.168Z",
+//       },
+//     ],
+//   },
+//   {
+//     cuid: 3,
+//     subject: null,
+//     users: [
+//       {
+//         // cuid: 1,
+//         username: "m@m.net",
+//         name: "Mine",
+//         bio: "SWE12",
+//       },
+//       {
+//         // cuid: 2,
+//         username: "m2@m.net",
+//         name: "Mine2",
+//         bio: null,
+//       },
+//     ],
+//     messages: [
+//       {
+//         cuid: 1,
+//         senderId: 1,
+//         content:
+//           "The first if statement will now execute and return a <div> with the text “Loading…” This is often the case when you are fetching from an API, since it might take some time to actually retrieve the data, it is good practice to show an indicator for that.",
+//         createdAt: "2026-07-12T17:48:37.168Z",
+//       },
+//       {
+//         cuid: 2,
+//         senderId: 1,
+//         content:
+//           "If none of those checks passed, then we have the data we need to render the list successfully. Try it out by adding items to the animals list and adding the property back.",
+//         createdAt: "2026-07-13T18:48:37.168Z",
+//       },
+//       {
+//         cuid: 3,
+//         senderId: 2,
+//         content:
+//           "You can, of course, also accomplish this with just the ternary and && operators.",
+//         createdAt: "2026-07-14T19:48:37.168Z",
+//       },
+//       {
+//         cuid: 4,
+//         senderId: 1,
+//         content:
+//           "The first if statement will now execute and return a <div> with the text “Loading…” This is often the case when you are fetching from an API, since it might take some time to actually retrieve the data, it is good practice to show an indicator for that.",
+//         createdAt: "2026-07-12T17:48:37.168Z",
+//       },
+//       {
+//         cuid: 5,
+//         senderId: 1,
+//         content:
+//           "If none of those checks passed, then we have the data we need to render the list successfully. Try it out by adding items to the animals list and adding the property back.",
+//         createdAt: "2026-07-13T18:48:37.168Z",
+//       },
+//       {
+//         cuid: 6,
+//         senderId: 2,
+//         content:
+//           "You can, of course, also accomplish this with just the ternary and && operators.",
+//         createdAt: "2026-07-14T19:48:37.168Z",
+//       },
+//     ],
+//   },
+// ];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,118 +4,9 @@ import Header from "../layouts/Header";
 import Chats from "../components/Chats";
 import Messages from "../components/Messages";
 import Profile from "../components/Profile";
-import styles from "../styles/Home.module.css";
 import GroupInfo from "../components/GroupInfo";
-
-// const chats = [
-//   {
-//     cuid: 1,
-//     subject: "Yids chat",
-//     users: [
-//       {
-//         // id: 1,
-//         username: "m@m.net",
-//         name: "Mine",
-//         bio: "The first if statement will now execute and return a <div> with the text “Loading…” This is often the case when you are fetching from an API, since it might take some time to actually retrieve the data, it is good practice to show an indicator for that. If none of those checks passed, then we have t",
-//       },
-//       {
-//         // cuid: 2,
-//         username: "m3@m.net",
-//         name: "Mine3",
-//         bio: null,
-//       },
-//     ],
-//     messages: [
-//       {
-//         cuid: 4,
-//         senderId: 1,
-//         content:
-//           "In our <List /> component, we have two if statements acting as a guard that immediately returns an element based on the condition.",
-//         createdAt: "2026-07-12T17:48:37.168Z",
-//       },
-//       {
-//         cuid: 5,
-//         senderId: 1,
-//         content:
-//           "One is to check if the property animals exists, and the other is to check if the length of the list is greater than 0. In this case, our list is empty, so the second if statement executes, which immediately returns the <div> element that contains the text “There are no animals in the list”.",
-//         createdAt: "2026-07-13T18:48:37.168Z",
-//       },
-//       {
-//         cuid: 6,
-//         senderId: 2,
-//         content: "If we remove the animals property:",
-//         createdAt: "2026-07-14T19:48:37.168Z",
-//       },
-//     ],
-//   },
-//   {
-//     cuid: 3,
-//     subject: null,
-//     users: [
-//       {
-//         // cuid: 1,
-//         username: "m@m.net",
-//         name: "Mine",
-//         bio: "SWE12",
-//       },
-//       {
-//         // cuid: 2,
-//         username: "m2@m.net",
-//         name: "Mine2",
-//         bio: null,
-//       },
-//     ],
-//     messages: [
-//       {
-//         cuid: 1,
-//         senderId: 1,
-//         content:
-//           "The first if statement will now execute and return a <div> with the text “Loading…” This is often the case when you are fetching from an API, since it might take some time to actually retrieve the data, it is good practice to show an indicator for that.",
-//         createdAt: "2026-07-12T17:48:37.168Z",
-//       },
-//       {
-//         cuid: 2,
-//         senderId: 1,
-//         content:
-//           "If none of those checks passed, then we have the data we need to render the list successfully. Try it out by adding items to the animals list and adding the property back.",
-//         createdAt: "2026-07-13T18:48:37.168Z",
-//       },
-//       {
-//         cuid: 3,
-//         senderId: 2,
-//         content:
-//           "You can, of course, also accomplish this with just the ternary and && operators.",
-//         createdAt: "2026-07-14T19:48:37.168Z",
-//       },
-//       {
-//         cuid: 4,
-//         senderId: 1,
-//         content:
-//           "The first if statement will now execute and return a <div> with the text “Loading…” This is often the case when you are fetching from an API, since it might take some time to actually retrieve the data, it is good practice to show an indicator for that.",
-//         createdAt: "2026-07-12T17:48:37.168Z",
-//       },
-//       {
-//         cuid: 5,
-//         senderId: 1,
-//         content:
-//           "If none of those checks passed, then we have the data we need to render the list successfully. Try it out by adding items to the animals list and adding the property back.",
-//         createdAt: "2026-07-13T18:48:37.168Z",
-//       },
-//       {
-//         cuid: 6,
-//         senderId: 2,
-//         content:
-//           "You can, of course, also accomplish this with just the ternary and && operators.",
-//         createdAt: "2026-07-14T19:48:37.168Z",
-//       },
-//     ],
-//   },
-// ];
-
-// remove ids
-// add name to messages
-// get profiles from chats?
-// how to deal with users leaving chats?
+import styles from "../styles/Home.module.css";
+import useProfile from "../hooks/useProfile";
 
 function Home({ setLoggedIn }) {
   const [currentChat, setCurrentChat] = useState(null);
@@ -123,10 +14,35 @@ function Home({ setLoggedIn }) {
   const [profileUser, setProfileUser] = useState(null);
   const [groupInfoChat, setGroupInfoChat] = useState(null);
 
+  const username = localStorage.getItem("MMA");
+  const { chats, error, loading } = useChats(username);
+  console.log(chats);
+  if (loading) return <h2>Loading...</h2>;
+  if (error) return <h2>A network error was encountered</h2>;
+
   const getProfileUser = (username) => {
-    if (!chats.length) {
-      console.log("get profile");
-    } else {
+    if (!chats.length && !profileUser) {
+      const apiUrl = import.meta.env.VITE_API_URL;
+      const JWT = localStorage.getItem("JWT");
+      console.log("fetching profile");
+
+      fetch(`${apiUrl}profile`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${JWT}`,
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ username }),
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then((response) => setProfileUser(response))
+        .catch((error) => console.error(error));
+    } else if (chats.length) {
       const user = chats
         .flatMap((obj) => obj.users)
         .find((user) => user.username === username);
@@ -139,13 +55,7 @@ function Home({ setLoggedIn }) {
     setGroupInfoChat(chat);
   };
 
-  const username = localStorage.getItem("MMA");
-  const { chats, error, loading } = useChats(username);
-  console.log(chats);
-  if (loading) return <h2>Loading...</h2>;
-  if (error) return <h2>A network error was encountered</h2>;
-
-  // apis - get profile. update profile,
+  // apis - update profile,
   // create chat, create message
   // add leave group/chat
 
